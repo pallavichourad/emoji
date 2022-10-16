0952a67229b299339d97ac54edf9cf7822808a8d
root <root@localhost>
2022-10-16 11:34:10 +0530

    auto publish 1665900249677

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Add all your JS here
+let fighters=["🐍","🐇","🐢","🐥","🐘","🐶","🐈",
+"🐪","🐿️","🐁"]
+let stageEl=document.getElementById("stage")
+let fightButton=document.getElementById("fightButton")
+fightButton.addEventListener("click", function(){
+  let randomIndexOne=Math.floor(Math.random()*fighters.length)
+  let randomIndexTwo=Math.floor(Math.random()*fighters.length)
+  stageEl.textContent= fighters[randomIndexOne] + "vs" + fighters[randomIndexTwo]
+})
